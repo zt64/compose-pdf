98c9804c1a6c962c11bd1d1c07a322fb19d83bfc
zt64 <31907977+zt64@users.noreply.github.com>
2025-03-22 11:50:00 -0400

    add unified pdf state

--- lib/src/commonMain/kotlin/dev/zt64/compose/pdf/component/PdfPage.kt
+++ lib/src/commonMain/kotlin/dev/zt64/compose/pdf/component/PdfPage.kt
@@ -1,69 +1,51 @@
 package dev.zt64.compose.pdf.component
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.size
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import dev.zt64.compose.pdf.LoadState
 import dev.zt64.compose.pdf.PdfState
-import dev.zt64.compose.pdf.RemotePdfState
 
+/**
+ * Pdf page
+ *
+ * @param state
+ * @param index
+ * @param modifier
+ * @param loadingIndicator
+ * @param errorIndicator
+ * @param contentScale
+ */
 @Composable
 public fun PdfPage(
     state: PdfState,
     index: Int,
     modifier: Modifier = Modifier,
-    loadingIconTint: Color = Color.Gray,
-    errorIconTint: Color = Color.Red,
-    iconSize: Dp = 40.dp,
-    loadingIndicator: @Composable () -> Unit = {
-        Image(
-            painter = state.renderPage(index),
-            contentDescription = null,
-            colorFilter = if (loadingIconTint ==
-                Color.Unspecified
-            ) {
-                null
-            } else {
-                ColorFilter.tint(loadingIconTint)
-            },
-            modifier = Modifier.size(iconSize)
-        )
-    },
-    errorIndicator: @Composable () -> Unit = {
-        Image(
-            painter = state.renderPage(index),
-            contentDescription = null,
-            colorFilter = if (errorIconTint ==
-                Color.Unspecified
-            ) {
-                null
-            } else {
-                ColorFilter.tint(errorIconTint)
-            },
-            modifier = Modifier.size(iconSize)
-        )
-    },
+    loadingIndicator: @Composable () -> Unit = {},
+    errorIndicator: @Composable () -> Unit = {},
     contentScale: ContentScale = ContentScale.Fit
 ) {
-    val loadState = if (state is RemotePdfState) state.loadState else LoadState.Success
+    val scope = rememberCoroutineScope()
 
+    val loadState by state.loadState.collectAsState()
     when (loadState) {
-        LoadState.Success -> Image(
-            modifier = modifier.background(Color.White),
-            painter = state.renderPage(index),
-            contentDescription = null,
-            contentScale = contentScale
-        )
-
         LoadState.Loading -> loadingIndicator()
 
-        LoadState.Error -> errorIndicator()
+        LoadState.Loaded -> {
+            val b = state.loadPage(index)
+            Canvas(
+                modifier = modifier
+                    .width(b.width.dp)
+                    .height(b.height.dp)
+            ) {
+                drawImage(b)
+            }
+        }
+
+        is LoadState.Error -> errorIndicator()
     }
 }
